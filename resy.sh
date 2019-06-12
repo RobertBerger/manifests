8b62eefda561a8f23f329e62fa5a7166158b64da
Robert Berger <robert.berger@ReliableEmbeddedSystems.com>
2019-06-12 23:04:12 +0300

    more layers

--- resy.sh
+++ resy.sh
@@ -9,10 +9,16 @@ pushd sources
 pushd poky
 git checkout -b 2019-05-13-warrior-2.7+
 popd
+pushd meta-multi-v7-ml-bsp
+git checkout -b warrior-v4.19.x
+popd
 popd
 
 pushd sources
 pushd poky
+git branch
+popd
+pushd meta-multi-v7-ml-bsp
 git branch
 popd
 popd
