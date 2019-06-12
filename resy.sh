e614516b173e64ab77f8bb0545ea0a89e082c489
Robert Berger <robert.berger@ReliableEmbeddedSystems.com>
2019-06-12 22:41:04 +0300

    test

---
+++ resy.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -x
+rm -rf .repo
+rm -rf sources
+repo init -u https://github.com/RobertBerger/manifests -m resy.xml
+repo sync
+
+pushd sources
+pushd poky
+git checkout -b 2019-05-13-warrior-2.7+
+popd
+popd
+
+pushd sources
+pushd poky
+git branch sources poky
+popd
+popd
+
+set +x
+
