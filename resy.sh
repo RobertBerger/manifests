78872f0d4813abe9aa25d16c79df2b56e86bc59c
Robert Berger <robert.berger@ReliableEmbeddedSystems.com>
2019-06-12 23:26:38 +0300

    new layer

--- resy.sh
+++ resy.sh
@@ -6,21 +6,33 @@ repo init -u https://github.com/RobertBerger/manifests -m resy.xml
 repo sync
 
 pushd sources
+
 pushd poky
 git checkout -b 2019-05-13-warrior-2.7+
 popd
 pushd meta-multi-v7-ml-bsp
 git checkout -b warrior-v4.19.x
 popd
+pushd meta-resy
+git checkout -b warrior
+popd
+
+# popd sources
 popd
 
 pushd sources
+
 pushd poky
 git branch
 popd
 pushd meta-multi-v7-ml-bsp
 git branch
 popd
+pushd meta-resy
+git branch
+popd
+
+# popd sources
 popd
 
 set +x
