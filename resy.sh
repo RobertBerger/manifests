72ee2d2dc3932977e31c74a0c380da4681be0208
Robert Berger <robert.berger@ReliableEmbeddedSystems.com>
2019-11-20 14:07:03 +0200

    meta-sca zeus branch

--- resy.sh
+++ resy.sh
@@ -86,8 +86,8 @@ git branch --set-upstream-to=gitlab/${META_U_BOOT_KARO_WIC_BSP_BRANCH} ${META_U_
 popd
 
 pushd meta-sca
-git checkout -b 2019-11-19-master-zeus-3.0+
-git branch --set-upstream-to=github/2019-11-19-master-zeus-3.0+ 2019-11-19-master-zeus-3.0+
+git checkout -b 2019-11-19-zeus
+git branch --set-upstream-to=github/2019-11-19-zeus 2019-11-19-zeus
 popd
 
 pushd meta-resy
