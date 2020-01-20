826b5c8984d9db7c105dd9cd58b0ea69564aeed0
Robert Berger <robert.berger@ReliableEmbeddedSystems.com>
2020-01-20 09:06:31 +0200

    add IP of mirror into container

--- resy-poky-container.sh
+++ resy-poky-container.sh
@@ -3,7 +3,8 @@ echo "In the container:"
 echo "source resy-cooker.sh"
 echo "Press <ENTER> to go on"
 read r
+MIRROR_IP=$(getent ahostsv4 mirror | awk '{print $1}' | head -1)
 set -x
 docker pull reliableembeddedsystems/poky-container:ubuntu-16.04
-docker run --rm -it -v ${HOME}/projects:/projects -v /opt:/nfs -v ${PWD}:${PWD} -v ${PWD}:/workdir reliableembeddedsystems/poky-container:ubuntu-16.04 --workdir=/workdir
+docker run --rm -it --add-host mirror:${MIRROR_IP} -v ${HOME}/projects:/projects -v /opt:/nfs -v ${PWD}:${PWD} -v ${PWD}:/workdir reliableembeddedsystems/poky-container:ubuntu-16.04 --workdir=/workdir
 set +x
