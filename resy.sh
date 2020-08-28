318d73444e715046b725687243e9393528479fc4
Robert Berger <robert.berger@ReliableEmbeddedSystems.com>
2020-08-28 11:51:37 +0300

    kept working set as bleeding

--- resy.sh
+++ resy.sh
@@ -51,16 +51,6 @@ else
   esac
 fi
 
-#set -x
-#rm -rf .repo
-#rm -rf sources
-#rm -rf scripts
-#rm -rf build
-#rm -rf app-container-x86-64
-#rm -rf crops-container-x86-64
-#rm -rf oci-container-x86-64
-#set +x
-
 # choose manifest
 
 if [[ $WORKSPACE = *jenkins* ]]; then
@@ -78,7 +68,6 @@ else
 fi
 
   if [ "$manifest" == "experimental" ]; then
-     #export MANIFEST="resy-experimental.xml"
      export META_RESY_BRANCH="dunfell"
      export META_POKY_BRANCH="2020-08-28-dunfell-3.1.2+"
      export META_POKY_TRAINING_BRANCH="2020-06-29-dunfell-3.1.1+"
@@ -109,27 +98,25 @@ fi
   fi
 
   if [ "$manifest" == "bleeding" ]; then
-     #export MANIFEST="resy-bleeding.xml"
-     export META_RESY_BRANCH="master"
-     export META_POKY_BRANCH="master"
+     export META_RESY_BRANCH="dunfell"
+     export META_POKY_BRANCH="2020-06-29-dunfell-3.1.1++"
      export META_POKY_TRAINING_BRANCH="2020-06-29-dunfell-3.1.1+"
-     export META_VIRTUALIZATION_BRANCH="2020-01-03-zeus-3.0+"
+     export META_VIRTUALIZATION_BRANCH="2020-04-30-dunfell-3.1"
      export META_DESIRE_BRANCH="master"
-     export META_MULTI_V7_ML_BSP_BRANCH="master"
-     export META_U_BOOT_WIC_BSP_BRANCH="master"
-     export META_U_BOOT_MENDER_BSP_BRANCH="master"
-     export META_U_BOOT_KARO_WIC_BSP_BRANCH="master"
-     export META_KARO_BSP_BRANCH="master"
-     export META_SCA_BRANCH="2020-01-03-zeus"
-     export META_OPENEMBEDDED_BRANCH="2020-01-03-zeus-3.0+"
-     export META_JAVA_BRANCH="2020-02-13-master-next-as-zeus-3.0+"
-     export META_TENSORFLOW_BRANCH="2020-05-10-master-as-zeus"
-     export MANIFESTS_BRANCH="master"
-     export META_PYTHON2_BRANCH="zeus"
-     export META_MENDER_BRANCH="2019-11-19-thud-as-zeus"
-     # not needed, since we still have python2 support
-     export META_PYTHON2_BRANCH="zeus"
-     export META_BFE_BRANCH="master"
+     export META_MULTI_V7_ML_BSP_BRANCH="dunfell"
+     export META_U_BOOT_WIC_BSP_BRANCH="dunfell"
+     export META_U_BOOT_MENDER_BSP_BRANCH="dunfell"
+     export META_U_BOOT_KARO_WIC_BSP_BRANCH="dunfell"
+     export META_KARO_BSP_BRANCH="dunfell-5.4.x"
+     export META_SCA_BRANCH="2020-06-05-dunfell"
+     export META_OPENEMBEDDED_BRANCH="2020-04-30-dunfell-3.1"
+     export META_JAVA_BRANCH="2020-05-08-dunfell"
+     export META_TENSORFLOW_BRANCH="2020-05-10-master-as-dunfell"
+     export MANIFESTS_BRANCH="dunfell"
+     export META_PYTHON2_BRANCH="dunfell"
+     export META_MENDER_BRANCH="2020-06-05-thud-as-dunfell"
+     export META_PYTHON2_BRANCH="dunfell"
+     export META_BFE_BRANCH="dunfell"
      export META_XENOMAI_BRANCH="xeno-3.1-4.19.128"
      export META_PRT_BRANCH="dunfell"
      export SKOPEO_BRANCH="skopeo-v1.1.0"
@@ -171,9 +158,6 @@ fi
      export META_FREESCALE_BRANCH="dunfell"
      export META_PHYBOARD_POLIS_IMX8MM_BSP_BRANCH="v5.8.0-upstream"
   fi
-#set -x
-#repo init -u https://github.com/RobertBerger/manifests -m ${MANIFEST}
-#repo sync
 
 declare -A MYMAP
 MYMAP[manifests]="${GITHUB}/RobertBerger/manifests ${SOURCES}/manifests ${MANIFESTS_BRANCH}"
@@ -182,6 +166,7 @@ MYMAP[poky]="${GITHUB}/RobertBerger/poky ${SOURCES}/poky ${META_POKY_BRANCH}"
 MYMAP[poky-training]="${GITHUB}/RobertBerger/poky ${SOURCES}/poky-training ${META_POKY_TRAINING_BRANCH}"
 # my-mender-layer (encrypted)
 MYMAP[my-mender-layer]="${GITHUB}/RobertBerger/my-mender-layer ${SOURCES}/my-mender-layer master"
+#MYMAP[keys-for-signing]="${GITHUB}/RobertBerger/keys-for-signing ${SOURCES}/keys-for-signing master"
 MYMAP[meta-python2]="${GITHUB}/RobertBerger/meta-python2 ${SOURCES}/meta-python2 ${META_PYTHON2_BRANCH}"
 MYMAP[meta-virtualization]="${GITHUB}/RobertBerger/meta-virtualization ${SOURCES}/meta-virtualization ${META_VIRTUALIZATION_BRANCH}"
 MYMAP[meta-wifi-credentials]="${GITHUB}/RobertBerger/meta-wifi-credentials ${SOURCES}/meta-wifi-credentials dunfell"
@@ -260,14 +245,6 @@ MYMAP[app-container-telegraf-prebuilt-oci-multi-arch]="${GITLAB}/app-container/a
 MYMAP[app-container-lighttpd-x86-64]="${GITLAB}/app-container/app-container-lighttpd.git ${APP_CONTAINER_X86_64}/app-container-lighttpd x86-64"
 MYMAP[app-container-lighttpd-multi-arch]="${GITLAB}/app-container/app-container-lighttpd.git ${APP_CONTAINER_MULTI_ARCH}/docker-lighttpd multi-arch"
 # <-- app-containers
-
-#if [ -d ${SOURCES} ]; then
-#   rm -rf ${SOURCES}
-#fi
-
-#mkdir ${SOURCES}
-
-#pushd ${SOURCES}
 
 for K in "${!MYMAP[@]}"
 do
@@ -396,7 +373,6 @@ fi
 #fi
 
 
-
 if [ ! -L resy.sh ]; then
    ln -sf sources/manifests/resy.sh resy.sh
 fi
@@ -406,310 +382,3 @@ set +x
 echo "symlinks ready"
 
 #read r
-
-#pushd poky
-#git checkout -b 2020-01-03-zeus-3.0.1+
-#git branch --set-upstream-to=github/2020-01-03-zeus-3.0.1+ 2020-01-03-zeus-3.0.1+
-#popd
-
-#pushd poky-training
-#git checkout -b 2020-01-03-zeus-3.0.1+
-#git branch --set-upstream-to=github/2020-01-03-zeus-3.0.1+ 2020-01-03-zeus-3.0.1+
-#popd
-
-#pushd my-mender-layer
-#git checkout -b master
-#git branch --set-upstream-to=github/master master
-#popd
-
-#pushd meta-virtualization
-#git checkout -b 2020-01-03-zeus-3.0+
-#git branch --set-upstream-to=github/2020-01-03-zeus-3.0+ 2020-01-03-zeus-3.0+
-#popd
-
-#pushd meta-u-boot-wic-bsp
-#git checkout -b ${META_U_BOOT_WIC_BSP_BRANCH}
-#git branch --set-upstream-to=gitlab/${META_U_BOOT_WIC_BSP_BRANCH} ${META_U_BOOT_WIC_BSP_BRANCH}
-#popd
-
-#pushd meta-u-boot-mender-bsp
-#git checkout -b ${META_U_BOOT_MENDER_BSP_BRANCH}
-#git branch --set-upstream-to=gitlab/${META_U_BOOT_MENDER_BSP_BRANCH} ${META_U_BOOT_MENDER_BSP_BRANCH}
-#popd
-
-#pushd meta-u-boot-karo-wic-bsp
-#git checkout -b ${META_U_BOOT_KARO_WIC_BSP_BRANCH}
-#git branch --set-upstream-to=gitlab/${META_U_BOOT_KARO_WIC_BSP_BRANCH} ${META_U_BOOT_KARO_WIC_BSP_BRANCH}
-#popd
-
-#pushd meta-sca
-#git checkout -b 2020-01-03-zeus
-#git branch --set-upstream-to=github/2020-01-03-zeus 2020-01-03-zeus
-#popd
-
-#pushd meta-resy
-#git checkout -b ${META_RESY_BRANCH}
-#git branch --set-upstream-to=gitlab/${META_RESY_BRANCH} ${META_RESY_BRANCH}
-#popd
-
-#pushd meta-openembedded
-#git checkout -b 2020-01-03-zeus-3.0+
-#git branch --set-upstream-to=github/2020-01-03-zeus-3.0+ 2020-01-03-zeus-3.0+
-#popd
-
-#pushd meta-multi-v7-ml-bsp
-#git checkout -b ${META_MULTI_V7_ML_BSP_BRANCH}
-#git branch --set-upstream-to=gitlab/${META_MULTI_V7_ML_BSP_BRANCH} ${META_MULTI_V7_ML_BSP_BRANCH}
-#popd
-
-#pushd meta-mender
-#git checkout -b 2019-11-19-thud-as-zeus
-#git branch --set-upstream-to=github/2019-11-19-thud-as-zeus 2019-11-19-thud-as-zeus
-#popd
-
-#pushd meta-karo-bsp
-#git checkout -b ${META_KARO_BSP_BRANCH}
-#git branch --set-upstream-to=gitlab/${META_KARO_BSP_BRANCH} ${META_KARO_BSP_BRANCH}
-#popd
-
-#pushd meta-desire
-#git checkout -b ${META_DESIRE_BRANCH}
-#git branch --set-upstream-to=gitlab/${META_DESIRE_BRANCH} ${META_DESIRE_BRANCH}
-#popd
-
-#pushd meta-clang
-#git checkout -b 2020-01-03-zeus
-#git branch --set-upstream-to=github/2020-01-03-zeus 2020-01-03-zeus
-#popd
-
-#pushd keys-for-signing
-#git checkout -b master
-#git branch --set-upstream-to=github/master master
-#popd
-
-#pushd meta-buildutils
-#git checkout -b 2020-01-03-zeus
-#git branch --set-upstream-to=github/2020-01-03-zeus 2020-01-03-zeus
-#popd
-
-#pushd meta-bfe
-#git checkout -b master
-#git branch --set-upstream-to=gitlab/master master
-#popd
-
-#pushd meta-java
-#git checkout -b 2020-02-13-master-next-as-zeus-3.0+
-#git branch --set-upstream-to=github/2020-02-13-master-next-as-zeus-3.0+ 2020-02-13-master-next-as-zeus-3.0+
-#popd
-
-# popd sources
-#popd
-
-#pushd scripts
-
-#pushd convenience-scripts
-#git checkout -b master
-#popd
-
-# popd scripts
-#popd
-
-#pushd crops-container-x86-64
-
-#pushd yocto-dockerfiles
-#git checkout -b 2019-11-19-master-local
-#git branch
-#popd
-
-#pushd poky-container
-#git checkout -b 2019-11-19-master-local
-#git branch
-#popd
-
-#pushd extsdk-container
-#git checkout -b 2020-01-06-master-local
-#git branch
-#popd
-
-# popd crops-container-x86-64
-#popd
-
-
-#pushd oci-container-x86-64
-
-#pushd skopeo-container
-#git checkout -b master
-#git branch
-#popd
-
-# popd oci-container-x86-64
-#popd
-
-#pushd app-container-x86-64
-
-#pushd app-container-redis
-#git checkout master
-#git branch
-#popd
-
-#pushd app-container-mosquitto
-#git checkout master
-#git branch
-#popd 
-
-#pushd app-container-python3-nmap-srv
-#git checkout master
-#git branch
-#popd
-
-#pushd app-container-python3-data-collector
-#git checkout master
-#git branch
-#popd
-
-#pushd app-container-image-lighttpd-oci
-#git checkout master
-#git branch
-#popd
-
-#pushd app-container-mosquitto-oci
-#git checkout master
-#git branch
-#popd
-
-#pushd app-container-oci
-#git checkout master
-#git branch
-#popd
-
-#pushd app-container-python3-data-collector-oci
-#git checkout master
-#git branch
-#popd
-
-#pushd app-container-python3-mastermind-oci
-#git checkout master
-#git branch
-#popd
-
-#pushd app-container-python3-mqttbrokerclient-oci
-#git checkout master
-#git branch
-#popd
-
-#pushd app-container-python3-nmap-srv-oci
-#git checkout master
-#git branch
-#popd
-
-#pushd app-container-redis-oci
-#git checkout master
-#git branch
-#popd
-
-# popd app-container-x86-64
-#popd
-
-#pushd sources
-
-#pushd poky
-#git branch
-#popd
-
-#pushd my-mender-layer
-#git branch
-#popd
-
-#pushd meta-virtualization
-#git branch
-#popd
-
-#pushd meta-u-boot-wic-bsp
-#git branch
-#popd
-
-#pushd meta-u-boot-mender-bsp
-#git branch
-#popd
-
-#pushd meta-u-boot-karo-wic-bsp
-#git branch
-#popd
-
-#pushd meta-sca
-#git branch
-#popd
-
-#pushd meta-resy
-#git branch
-#popd
-
-#pushd meta-openembedded
-#git branch
-#popd
-
-#pushd meta-multi-v7-ml-bsp
-#git branch
-#popd
-
-#pushd meta-mender
-#git branch
-#popd
-
-#pushd meta-karo-bsp
-#git branch
-#popd
-
-#pushd meta-desire
-#git branch
-#popd
-
-#pushd meta-clang
-#git branch
-#popd
-
-#pushd keys-for-signing
-#git branch
-#popd
-
-#pushd meta-buildutils
-#git branch
-#popd
-
-#pushd meta-bfe
-#git branch
-#popd
-
-# popd sources
-#popd
-
-#rm -f resy-cooker.sh
-#wget https://raw.githubusercontent.com/RobertBerger/manifests/master/resy-cooker.sh
-#chmod +x resy-cooker.sh
-
-#rm -f resy-poky-container.sh
-#wget https://raw.githubusercontent.com/RobertBerger/manifests/master/resy-poky-container.sh
-#chmod +x resy-poky-container.sh
-
-#rm -f resy-sdk-container-installer.sh
-#wget https://raw.githubusercontent.com/RobertBerger/manifests/master/resy-sdk-container-installer.sh
-#chmod +x resy-sdk-container-installer.sh
-
-#rm -f resy-sdk-container.sh
-#wget https://raw.githubusercontent.com/RobertBerger/manifests/master/resy-sdk-container.sh
-#chmod +x resy-sdk-container.sh
-
-#rm -f oci-copy-to-docker.sh
-#wget https://raw.githubusercontent.com/RobertBerger/manifests/master/oci-copy-to-docker.sh
-#chmod +x oci-copy-to-docker.sh
-
-#set +x
-
-#echo "new resy.sh:"
-#echo "rm -f resy.sh.* && wget --no-cache https://raw.githubusercontent.com/RobertBerger/manifests/master/resy.sh && chmod +x resy.sh"
-#echo "use resy-poky-container.sh to start poky build container"
-#echo "use resy-cooker.sh inside poky container to set up cooker and bitbake"
-
-#set +x
-
-
