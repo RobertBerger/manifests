d0d3ec2828f0318c32d221dd31329e2e25d59859
Robert Berger <robert.berger@ReliableEmbeddedSystems.com>
2020-10-25 12:25:45 +0200

    search for only bitbake/bin/bitbake

--- killall_bitbake.sh
+++ killall_bitbake.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 # kill all instances of bitbake
+#set -x
 ps -ef | grep bitbake
-for pid in $(ps -ef | awk '/\/poky\/bitbake\/bin\/bitbake/ {print $2}'); do kill -9 $pid; done
+#for pid in $(ps -ef | awk '/\/poky\/bitbake\/bin\/bitbake/ {print $2}'); do
+for pid in $(ps -ef | awk '/\/bitbake\/bin\/bitbake/ {print $2}'); do
+        echo "do kill -9 ${pid}";
+        kill -9 $pid;
+done
 ps -ef | grep bitbake
 echo "rm -f ${BUILDDIR}/hashserve.sock"
 rm -f ${BUILDDIR}/hashserve.sock
+#set +x
