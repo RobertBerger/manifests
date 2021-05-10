4b1331005ba0246ba24f043c0e5097cafbcb476d
Robert Berger <robert.berger@ReliableEmbeddedSystems.com>
2021-05-10 13:50:13 +0300

    updated patch

--- meta-xilinx-master/patch.sh
+++ meta-xilinx-master/patch.sh
@@ -2,9 +2,11 @@
 SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 echo ${SCRIPTPATH}
 set -x
+pushd meta-xilinx-bsp
 git checkout conf/layer.conf
 #if ! patch -R -p1 -s -f --dry-run <${SCRIPTPATH}/0001-add-hardknott.patch; then
   patch -p1 <${SCRIPTPATH}/0001-add-hardknott.patch
 #fi
 git diff --stat
+popd
 set +x
