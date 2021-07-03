01ba80ad8860f52c61c2e9e804d3853bb44c3a3e
Robert Berger <robert.berger@ReliableEmbeddedSystems.com>
2021-07-03 16:50:36 +0300

    resy-sdk-container: name it

--- resy-sdk-container.sh
+++ resy-sdk-container.sh
@@ -5,5 +5,5 @@ CONTAINER="reliableembeddedsystems/sdk-container:2021-05-31-master-local-gcc-9-u
 GITPOD_CMD="--add-host gitpod:192.168.42.40"
 set -x
 docker pull ${CONTAINER}
-docker run ${GITPOD_CMD} -e TARGET_UID=$(id -u ${USER}) -e TARGET_GID=$(stat -c "%g" /home/${USER}) -v /home/${USER}/yocto-download:/home/sdk/yocto-download -v /home/${USER}:/home/sdk -v /opt:/opt -v /workdir:/workdir -v /home/${USER}/projects:/projects  -v /tftpboot:/tftpboot --interactive --tty --entrypoint=/usr/bin/entrypoint.sh ${CONTAINER} --login
+docker run ${GITPOD_CMD} --name sdk-container -e TARGET_UID=$(id -u ${USER}) -e TARGET_GID=$(stat -c "%g" /home/${USER}) -v /home/${USER}/yocto-download:/home/sdk/yocto-download -v /home/${USER}:/home/sdk -v /opt:/opt -v /workdir:/workdir -v /home/${USER}/projects:/projects  -v /tftpboot:/tftpboot --interactive --tty --entrypoint=/usr/bin/entrypoint.sh ${CONTAINER} --login
 set +x
