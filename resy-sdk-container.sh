b352a802d2e1860af4fdccca5858983a851c22c2
Robert Berger <robert.berger@ReliableEmbeddedSystems.com>
2021-07-26 20:31:22 +0300

    kill sdk container if it is already running

--- resy-sdk-container.sh
+++ resy-sdk-container.sh
@@ -3,6 +3,21 @@
 #CONTAINER="reslocal/sdk-container:latest"
 CONTAINER="reliableembeddedsystems/sdk-container:2021-05-31-master-local-gcc-9-ub18"
 GITPOD_CMD="--add-host gitpod:192.168.42.40"
+
+# remove currently running containers
+echo "+ ID_TO_KILL=\$(docker ps -a -q  --filter ancestor=${CONTAINER})"
+ID_TO_KILL=$(docker ps -a -q  --filter ancestor=${CONTAINER})
+
+echo "+ docker ps -a"
+docker ps -a
+echo "+ docker stop ${ID_TO_KILL}"
+docker stop ${ID_TO_KILL}
+echo "+ docker rm -f ${ID_TO_KILL}"
+docker rm -f ${ID_TO_KILL}
+echo "+ docker ps -a"
+docker ps -a
+
+
 set -x
 docker pull ${CONTAINER}
 docker run ${GITPOD_CMD} --name sdk-container -e TARGET_UID=$(id -u ${USER}) -e TARGET_GID=$(stat -c "%g" /home/${USER}) -v /home/${USER}/yocto-download:/home/sdk/yocto-download -v /home/${USER}:/home/sdk -v /opt:/opt -v /workdir:/workdir -v /home/${USER}/projects:/projects  -v /tftpboot:/tftpboot --interactive --tty --entrypoint=/usr/bin/entrypoint.sh ${CONTAINER} --login
