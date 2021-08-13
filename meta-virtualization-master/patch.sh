8d4a9d53d0c26549df0495c36a7b065f25a7795f
Robert Berger <robert.berger@ReliableEmbeddedSystems.com>
2021-08-13 20:51:32 +0300

    don't apply patch at the moment

--- meta-virtualization-master/patch.sh
+++ meta-virtualization-master/patch.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
-SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
-echo ${SCRIPTPATH}
-set -x
-git checkout recipes-kernel/linux/linux-yocto_virtualization.inc
-#if ! patch -R -p1 -s -f --dry-run <${SCRIPTPATH}/0001-include-virtio.scc.patch; then
-  patch -p1 <${SCRIPTPATH}/0001-include-virtio.scc.patch
-#fi
-git diff --stat
-set +x
+# SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+# echo ${SCRIPTPATH}
+# set -x
+# git checkout recipes-kernel/linux/linux-yocto_virtualization.inc
+#  patch -p1 <${SCRIPTPATH}/0001-include-virtio.scc.patch
+# git diff --stat
+# set +x
