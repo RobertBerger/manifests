1af0eb8046ec545b0724ce7d8b65c687ca7a63ee
Robert Berger <robert.berger@ReliableEmbeddedSystems.com>
2021-09-02 19:10:21 +0300

    poky-container, yocto-dockerfiles - use latest

--- resy.sh
+++ resy.sh
@@ -393,8 +393,14 @@ MYMAP[terrier-container]="${GITHUB}/RobertBerger/terrier-container ${OCI_CONTAIN
 # extsdk-container is deprecated and replaced by sdk-container
 MYMAP[extsdk-container]="${GITHUB}/RobertBerger/extsdk-container ${CROPS_CONTAINER_X86_64}/extsdk-container 2020-09-17-master-local"
 MYMAP[sdk-container]="${GITHUB}/RobertBerger/sdk-container ${CROPS_CONTAINER_X86_64}/sdk-container 2021-05-31-master-local-gcc-9-ub18-esdk-exp"
-MYMAP[poky-container]="${GITHUB}/RobertBerger/poky-container ${CROPS_CONTAINER_X86_64}/poky-container 2021-06-09-master-local-gcc-9-gui-icecc-ub18"
-MYMAP[yocto-dockerfiles]="${GITHUB}/RobertBerger/yocto-dockerfiles ${CROPS_CONTAINER_X86_64}/yocto-dockerfiles 2020-05-30-master-local-ub18"
+# for training:
+# MYMAP[poky-container]="${GITHUB}/RobertBerger/poky-container ${CROPS_CONTAINER_X86_64}/poky-container 2021-06-09-master-local-gcc-9-gui-icecc-ub18"
+# new:
+MYMAP[poky-container]="${GITHUB}/RobertBerger/poky-container ${CROPS_CONTAINER_X86_64}/poky-container 2021-08-31-master-local-icecc-ub20"
+# for training:
+# MYMAP[yocto-dockerfiles]="${GITHUB}/RobertBerger/yocto-dockerfiles ${CROPS_CONTAINER_X86_64}/yocto-dockerfiles 2020-05-30-master-local-ub18"
+# new:
+MYMAP[yocto-dockerfiles]="${GITHUB}/RobertBerger/yocto-dockerfiles ${CROPS_CONTAINER_X86_64}/yocto-dockerfiles 2021-08-31-master-local-ub20"
 MYMAP[icecc-container]="${GITHUB}/RobertBerger/icecc-container ${CROPS_CONTAINER_X86_64}/icecc-container 2021-04-08"
 # <-- crops
 
