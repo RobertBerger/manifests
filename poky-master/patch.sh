5200ba06905c6f2839ac82c08f9720ac238c3721
Robert Berger <robert.berger@ReliableEmbeddedSystems.com>
2021-09-02 19:09:37 +0300

    currently no patch necessary for poky-master

--- poky-master/patch.sh
+++ poky-master/patch.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
-echo ${SCRIPTPATH}
-set -x
-#git checkout recipes-kernel/linux/linux-yocto_virtualization.inc
-git checkout meta/classes/package_pkgdata.bbclass
-#if ! patch -R -p1 -s -f --dry-run <${SCRIPTPATH}/0001-include-virtio.scc.patch; then
-  patch -p1 <${SCRIPTPATH}/0001-exclude-SSTATETASKS-from-package_prepare_pkgdata.patch
-#fi
-git diff --stat
-set +x
+#SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+#echo ${SCRIPTPATH}
+#set -x
+##git checkout recipes-kernel/linux/linux-yocto_virtualization.inc
+#git checkout meta/classes/package_pkgdata.bbclass
+##if ! patch -R -p1 -s -f --dry-run <${SCRIPTPATH}/0001-include-virtio.scc.patch; then
+#  patch -p1 <${SCRIPTPATH}/0001-exclude-SSTATETASKS-from-package_prepare_pkgdata.patch
+##fi
+#git diff --stat
+#set +x
