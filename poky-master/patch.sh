993c8a6366fab3b42aa0aebdafc9a727ef671f0f
Robert Berger <robert.berger@ReliableEmbeddedSystems.com>
2022-06-22 15:50:38 +0200

    fix resolfconf

--- poky-master/patch.sh
+++ poky-master/patch.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-#SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
-#echo ${SCRIPTPATH}
-#set -x
-##git checkout recipes-kernel/linux/linux-yocto_virtualization.inc
-#git checkout meta/classes/package_pkgdata.bbclass
-##if ! patch -R -p1 -s -f --dry-run <${SCRIPTPATH}/0001-include-virtio.scc.patch; then
-#  patch -p1 <${SCRIPTPATH}/0001-exclude-SSTATETASKS-from-package_prepare_pkgdata.patch
-##fi
-#git diff --stat
-#set +x
+SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+echo ${SCRIPTPATH}
+set -x
+git checkout meta/recipes-connectivity/resolvconf/resolvconf_1.91.bb
+if ! patch -R -p1 -s -f --dry-run <${SCRIPTPATH}/0004-normalize-resolvconf-needs-to-be-installed.patch; then
+     patch -p1 <${SCRIPTPATH}/0004-normalize-resolvconf-needs-to-be-installed.patch
+fi
+git diff --stat
+set +x
