4905ceda26e87c09fe1e7a07d31580987b82740a
Robert Berger <robert.berger@ReliableEmbeddedSystems.com>
2022-09-12 09:20:29 +0200

    patch also recipes-extended/images/update-image.bb

--- meta-swupdate-boards-master/patch.sh
+++ meta-swupdate-boards-master/patch.sh
@@ -6,5 +6,9 @@ git checkout conf/layer.conf
 if ! patch -R -p1 -s -f --dry-run <${SCRIPTPATH}/against-4189ecc0cfc26ffba5975159f33ea48807a0408c/0001-added-langdale-kirkstone.patch; then
      patch -p1 <${SCRIPTPATH}/against-4189ecc0cfc26ffba5975159f33ea48807a0408c/0001-added-langdale-kirkstone.patch
 fi
+git checkout recipes-extended/images/update-image.bb
+if ! patch -R -p1 -s -f --dry-run <${SCRIPTPATH}/against-4189ecc0cfc26ffba5975159f33ea48807a0408c/0002-soft-assignment-so-we-can-override.patch; then
+     patch -p1 <${SCRIPTPATH}/against-4189ecc0cfc26ffba5975159f33ea48807a0408c/0002-soft-assignment-so-we-can-override.patch
+fi
 git diff --stat
 set +x
