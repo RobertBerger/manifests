e10da1b7107781fd239711bb44c4c4c375cb3bf9
Robert Berger <robert.berger@ReliableEmbeddedSystems.com>
2022-10-11 22:13:29 +0200

    std. scripts - now also untracked files/symlinks are recognized and copied over

--- scripts/remote-scp-untracked-files.sh
+++ scripts/remote-scp-untracked-files.sh
@@ -4,9 +4,10 @@ set -x
 rm -rf /tmp/untracked
 mkdir /tmp/untracked
 cd ../
-git ls-files --others | xargs -I {} cp --parents {} /tmp/untracked
-tree /tmp/untracked
+#git ls-files --others | xargs -I {} cp --parents {} /tmp/untracked
+git ls-files --others | xargs -I {} rsync ./{} /tmp/untracked -avp --relative
+tree -C /tmp/untracked
 cd /tmp/untracked
-tar czvf ../untracked.tar.gz .
+tar czf ../untracked.tar.gz .
 scp ../untracked.tar.gz ${SFTPSERVER}
 set +x
--- scripts/scp-untracked-files.sh
+++ scripts/scp-untracked-files.sh
@@ -2,8 +2,9 @@
 rm -rf /tmp/untracked
 mkdir /tmp/untracked
 cd ../
-git ls-files --others | xargs -I {} cp --parents {} /tmp/untracked
-tree /tmp/untracked
+#git ls-files --others | xargs -I {} cp --parents {} /tmp/untracked
+git ls-files --others | xargs -I {} rsync ./{} /tmp/untracked -avp --relative
+tree -C /tmp/untracked
 cd /tmp/untracked
-tar czvf ../untracked.tar.gz .
+tar czf ../untracked.tar.gz .
 scp ../untracked.tar.gz rber@192.168.42.52:/tmp
