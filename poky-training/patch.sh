52d9268d4534ca44ce464a1a010e685059394d95
Robert Berger <Robert.Berger@ReliableEmbeddedSystems.com>
2023-06-30 18:19:21 +0200

    uninative-tarball.xz-reproducibility-fix

--- poky-training/patch.sh
+++ poky-training/patch.sh
@@ -4,8 +4,8 @@ echo ${SCRIPTPATH}
 set -x
 
 git checkout meta/classes-recipe/populate_sdk_base.bbclass
-if ! patch -R -p1 -s -f --dry-run <${SCRIPTPATH}/against-f21c1dd4f46ffcc951e08e9b94256ba6ee97b670/0001-SDKTAROPTS_ADDON.patch; then
-     patch -p1 <${SCRIPTPATH}/against-f21c1dd4f46ffcc951e08e9b94256ba6ee97b670/0001-SDKTAROPTS_ADDON.patch
+if ! patch -R -p1 -s -f --dry-run <${SCRIPTPATH}/against-f21c1dd4f46ffcc951e08e9b94256ba6ee97b670/0001-uninative-tarball.xz-reproducibility-fix.patch; then
+     patch -p1 <${SCRIPTPATH}/against-f21c1dd4f46ffcc951e08e9b94256ba6ee97b670/0001-uninative-tarball.xz-reproducibility-fix.patch
 fi
 
 git diff --stat
