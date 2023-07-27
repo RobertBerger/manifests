bda048031f82f97df12b3d4efeada43f5dda12ae
Robert Berger <Robert.Berger@ReliableEmbeddedSystems.com>
2023-07-27 15:03:16 +0200

    prepare new master

--- poky-master/patch.sh
+++ poky-master/patch.sh
@@ -4,22 +4,32 @@ echo ${SCRIPTPATH}
 set -x
 
 git checkout meta/recipes-connectivity/connman/connman/connman
-if ! patch -R -p1 -s -f --dry-run <${SCRIPTPATH}/against-d221e59a5067266c3f620259a1e56a56823df1fb/0001-connman-allow-rootfs-over-nfs.patch; then
-     patch -p1 <${SCRIPTPATH}/against-d221e59a5067266c3f620259a1e56a56823df1fb/0001-connman-allow-rootfs-over-nfs.patch
+if ! patch -R -p1 -s -f --dry-run <${SCRIPTPATH}/against-3eff0eb5ea77de20d85a2ffc64652579cbd7755c/0001-connman-allow-rootfs-over-nfs.patch; then
+     patch -p1 <${SCRIPTPATH}/against-3eff0eb5ea77de20d85a2ffc64652579cbd7755c/0001-connman-allow-rootfs-over-nfs.patch
 fi
 
 git checkout meta-poky/conf/distro/poky.conf
-if ! patch -R -p1 -s -f --dry-run <${SCRIPTPATH}/against-d221e59a5067266c3f620259a1e56a56823df1fb/0002-poky-snapshot-.66.patch; then
-     patch -p1 <${SCRIPTPATH}/against-d221e59a5067266c3f620259a1e56a56823df1fb/0002-poky-snapshot-.66.patch
+if ! patch -R -p1 -s -f --dry-run <${SCRIPTPATH}/against-3eff0eb5ea77de20d85a2ffc64652579cbd7755c/0002-poky-snapshot-.66.patch; then
+     patch -p1 <${SCRIPTPATH}/against-3eff0eb5ea77de20d85a2ffc64652579cbd7755c/0002-poky-snapshot-.66.patch
 fi
 
 git checkout meta/recipes-connectivity/resolvconf/resolvconf_1.91.bb
-if ! patch -R -p1 -s -f --dry-run <${SCRIPTPATH}/against-d221e59a5067266c3f620259a1e56a56823df1fb/0003-RDEPENDS-skip-e.g.-normalize-resolvconf-with-busybox.patch; then
-     patch -p1 <${SCRIPTPATH}/against-d221e59a5067266c3f620259a1e56a56823df1fb/0003-RDEPENDS-skip-e.g.-normalize-resolvconf-with-busybox.patch
+if ! patch -R -p1 -s -f --dry-run <${SCRIPTPATH}/against-3eff0eb5ea77de20d85a2ffc64652579cbd7755c/0003-RDEPENDS-skip-e.g.-normalize-resolvconf-with-busybox.patch; then
+     patch -p1 <${SCRIPTPATH}/against-3eff0eb5ea77de20d85a2ffc64652579cbd7755c/0003-RDEPENDS-skip-e.g.-normalize-resolvconf-with-busybox.patch
 fi
 
-if ! patch -R -p1 -s -f --dry-run <${SCRIPTPATH}/against-d221e59a5067266c3f620259a1e56a56823df1fb/0004-resolveconf-more-explanations-still-unclear.patch; then
-     patch -p1 <${SCRIPTPATH}/against-d221e59a5067266c3f620259a1e56a56823df1fb/0004-resolveconf-more-explanations-still-unclear.patch
+if ! patch -R -p1 -s -f --dry-run <${SCRIPTPATH}/against-3eff0eb5ea77de20d85a2ffc64652579cbd7755c/0004-resolveconf-more-explanations-still-unclear.patch; then
+     patch -p1 <${SCRIPTPATH}/against-3eff0eb5ea77de20d85a2ffc64652579cbd7755c/0004-resolveconf-more-explanations-still-unclear.patch
+fi
+
+git checkout meta/classes-recipe/populate_sdk_base.bbclass
+if ! patch -R -p1 -s -f --dry-run <${SCRIPTPATH}/against-3eff0eb5ea77de20d85a2ffc64652579cbd7755c/0009-uninative-tarball.xz-reproducibility-fix.patch; then
+     patch -p1 <${SCRIPTPATH}/against-3eff0eb5ea77de20d85a2ffc64652579cbd7755c/0009-uninative-tarball.xz-reproducibility-fix.patch
+fi
+
+git checkout scripts/lib/wic/plugins/source/bootimg-pcbios.py
+if ! patch -R -p1 -s -f --dry-run <${SCRIPTPATH}/against-3eff0eb5ea77de20d85a2ffc64652579cbd7755c/0010-optionally-pass-initrd-from-.wks-.in-via-sourceparam.patch; then
+     patch -p1 <${SCRIPTPATH}/against-3eff0eb5ea77de20d85a2ffc64652579cbd7755c/0010-optionally-pass-initrd-from-.wks-.in-via-sourceparam.patch
 fi
 
 git diff --stat
