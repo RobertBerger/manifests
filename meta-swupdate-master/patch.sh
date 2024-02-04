65669a48cf5435658b224dfea2c3786fd50e3ffe
Robert Berger <Robert.Berger@ReliableEmbeddedSystems.com>
2024-02-04 20:52:41 +0100

    meta-swupdate-master: git add/commit patch

--- meta-swupdate-master/patch.sh
+++ meta-swupdate-master/patch.sh
@@ -7,5 +7,10 @@ git checkout recipes-support/swupdate/swupdate/swupdate.socket.tmpl
 if ! patch -R -p1 -s -f --dry-run <${SCRIPTPATH}/against-ea21eccbd117da250d04430155ed61b39042835a/0001-updated-swupdate.service-swupdate.socket.tmpl.patch; then
      patch -p1 <${SCRIPTPATH}/against-ea21eccbd117da250d04430155ed61b39042835a/0001-updated-swupdate.service-swupdate.socket.tmpl.patch
 fi
+
 git diff --stat
+
+git add .
+git commit -m "patch typically only against upstream master"
+
 set +x
