a17b26d9f84f08d5340922096b5fd4db2e7ab9fb
Robert Berger <Robert.Berger@ReliableEmbeddedSystems.com>
2024-06-18 15:15:17 +0200

    meta-swupdate-boards-master updated

--- meta-swupdate-boards-master/patch.sh
+++ meta-swupdate-boards-master/patch.sh
@@ -3,12 +3,12 @@ SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 echo ${SCRIPTPATH}
 set -x
 git checkout conf/layer.conf
-if ! patch -R -p1 -s -f --dry-run <${SCRIPTPATH}/against-965bcd8da5ad2174bd2ac05811580e44bbb0e036/0001-nanbield-support.patch; then
-     patch -p1 <${SCRIPTPATH}/against-965bcd8da5ad2174bd2ac05811580e44bbb0e036/0001-nanbield-support.patch
+if ! patch -R -p1 -s -f --dry-run <${SCRIPTPATH}/against-df7a843eba850882fe0b843d827314d939267395/0001-update-to-scarthgap-styhead.patch; then
+     patch -p1 <${SCRIPTPATH}/against-df7a843eba850882fe0b843d827314d939267395/0001-update-to-scarthgap-styhead.patch
 fi
 git checkout recipes-extended/images/update-image.bb
-if ! patch -R -p1 -s -f --dry-run <${SCRIPTPATH}/against-965bcd8da5ad2174bd2ac05811580e44bbb0e036/0002-soft-assignment-so-we-can-override.patch; then
-     patch -p1 <${SCRIPTPATH}/against-965bcd8da5ad2174bd2ac05811580e44bbb0e036/0002-soft-assignment-so-we-can-override.patch
+if ! patch -R -p1 -s -f --dry-run <${SCRIPTPATH}/against-df7a843eba850882fe0b843d827314d939267395/0002-soft-assignment-so-we-can-override.patch; then
+     patch -p1 <${SCRIPTPATH}/against-df7a843eba850882fe0b843d827314d939267395/0002-soft-assignment-so-we-can-override.patch
 fi
 git diff --stat
 
