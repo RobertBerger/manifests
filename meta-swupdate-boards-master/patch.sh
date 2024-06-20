f852739d3241aa1c0758ef44295c804d156ac65c
Robert Berger <Robert.Berger@ReliableEmbeddedSystems.com>
2024-06-20 17:15:36 +0200

    meta-swupdate-boards-master: updated

--- meta-swupdate-boards-master/patch.sh
+++ meta-swupdate-boards-master/patch.sh
@@ -2,14 +2,27 @@
 SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 echo ${SCRIPTPATH}
 set -x
+
 git checkout conf/layer.conf
-if ! patch -R -p1 -s -f --dry-run <${SCRIPTPATH}/against-df7a843eba850882fe0b843d827314d939267395/0001-update-to-scarthgap-styhead.patch; then
-     patch -p1 <${SCRIPTPATH}/against-df7a843eba850882fe0b843d827314d939267395/0001-update-to-scarthgap-styhead.patch
+if ! patch -R -p1 -s -f --dry-run <${SCRIPTPATH}/against-965bcd8da5ad2174bd2ac05811580e44bbb0e036/0001-update-to-scarthgap-styhead.patch; then
+     patch -p1 <${SCRIPTPATH}/against-965bcd8da5ad2174bd2ac05811580e44bbb0e036/0001-update-to-scarthgap-styhead.patch
 fi
+
 git checkout recipes-extended/images/update-image.bb
-if ! patch -R -p1 -s -f --dry-run <${SCRIPTPATH}/against-df7a843eba850882fe0b843d827314d939267395/0002-soft-assignment-so-we-can-override.patch; then
-     patch -p1 <${SCRIPTPATH}/against-df7a843eba850882fe0b843d827314d939267395/0002-soft-assignment-so-we-can-override.patch
+if ! patch -R -p1 -s -f --dry-run <${SCRIPTPATH}/against-965bcd8da5ad2174bd2ac05811580e44bbb0e036/0002-soft-assignment-so-we-can-override.patch; then
+     patch -p1 <${SCRIPTPATH}/against-965bcd8da5ad2174bd2ac05811580e44bbb0e036/0002-soft-assignment-so-we-can-override.patch
 fi
+
+git checkout recipes-bsp/libubootenv/libubootenv_%.bbappend
+if ! patch -R -p1 -s -f --dry-run <${SCRIPTPATH}/against-965bcd8da5ad2174bd2ac05811580e44bbb0e036/0003-libubootenv-WORKDIR-UNPACKDIR.patch; then
+     patch -p1 <${SCRIPTPATH}/against-965bcd8da5ad2174bd2ac05811580e44bbb0e036/0003-libubootenv-WORKDIR-UNPACKDIR.patch
+fi
+
+git checkout recipes-support/swupdate/swupdate_%.bbappend
+if ! patch -R -p1 -s -f --dry-run <${SCRIPTPATH}/against-965bcd8da5ad2174bd2ac05811580e44bbb0e036/0004-swupdate-WORKDIR-UNPACKDIR.patch; then
+     patch -p1 <${SCRIPTPATH}/against-965bcd8da5ad2174bd2ac05811580e44bbb0e036/0004-swupdate-WORKDIR-UNPACKDIR.patch
+fi
+
 git diff --stat
 
 git add .
