f6f816dcc73efeff51135b9fe43eeeb805a0d44d
Robert Berger <Robert.Berger@ReliableEmbeddedSystems.com>
2024-06-20 17:16:00 +0200

    meta-swupdate-master: updated

--- meta-swupdate-master/patch.sh
+++ meta-swupdate-master/patch.sh
@@ -2,11 +2,51 @@
 SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 echo ${SCRIPTPATH}
 set -x
-git checkout recipes-support/swupdate/swupdate/swupdate.service
-git checkout recipes-support/swupdate/swupdate/swupdate.socket.tmpl
-if ! patch -R -p1 -s -f --dry-run <${SCRIPTPATH}/against-ea21eccbd117da250d04430155ed61b39042835a/0001-updated-swupdate.service-swupdate.socket.tmpl.patch; then
-     patch -p1 <${SCRIPTPATH}/against-ea21eccbd117da250d04430155ed61b39042835a/0001-updated-swupdate.service-swupdate.socket.tmpl.patch
+
+git checkout classes-recipe/swupdate-common.bbclass
+if ! patch -R -p1 -s -f --dry-run <${SCRIPTPATH}/against-5a82bce0d58d9b019e1ab23fcf1224a982168d80/0001-swupdate-common-bbclass-5.1-UNPACKDIR.patch; then
+     patch -p1 <${SCRIPTPATH}/against-5a82bce0d58d9b019e1ab23fcf1224a982168d80/0001-swupdate-common-bbclass-5.1-UNPACKDIR.patch
 fi
+
+git checkout classes-recipe/swupdate-image.bbclass
+if ! patch -R -p1 -s -f --dry-run <${SCRIPTPATH}/against-5a82bce0d58d9b019e1ab23fcf1224a982168d80/0002-swupdate-image-bbclass-5.1-UNPACKDIR.patch; then
+     patch -p1 <${SCRIPTPATH}/against-5a82bce0d58d9b019e1ab23fcf1224a982168d80/0002-swupdate-image-bbclass-5.1-UNPACKDIR.patch
+fi
+
+git checkout classes-recipe/swupdate.bbclass
+if ! patch -R -p1 -s -f --dry-run <${SCRIPTPATH}/against-5a82bce0d58d9b019e1ab23fcf1224a982168d80/0003-swupdate-bbclass-5.1-UNPACKDIR.patch; then
+     patch -p1 <${SCRIPTPATH}/against-5a82bce0d58d9b019e1ab23fcf1224a982168d80/0003-swupdate-bbclass-5.1-UNPACKDIR.patch
+fi
+
+git checkout recipes-lua/swupdate-lualoader/swupdate-lualoader_1.0.bb
+if ! patch -R -p1 -s -f --dry-run <${SCRIPTPATH}/against-5a82bce0d58d9b019e1ab23fcf1224a982168d80/0004-swupdate-lualoader-5.1-UNPACKDIR.patch; then
+     patch -p1 <${SCRIPTPATH}/against-5a82bce0d58d9b019e1ab23fcf1224a982168d80/0004-swupdate-lualoader-5.1-UNPACKDIR.patch
+fi
+
+git checkout recipes-support/swupdate/swupdate.inc
+if ! patch -R -p1 -s -f --dry-run <${SCRIPTPATH}/against-5a82bce0d58d9b019e1ab23fcf1224a982168d80/0005-swupdate-inc-5.1-UNPACKDIR.patch; then
+     patch -p1 <${SCRIPTPATH}/against-5a82bce0d58d9b019e1ab23fcf1224a982168d80/0005-swupdate-inc-5.1-UNPACKDIR.patch
+fi
+
+# --> RES specific stuff
+
+git checkout recipes-support/swupdate/swupdate/swupdate.service
+if ! patch -R -p1 -s -f --dry-run <${SCRIPTPATH}/against-5a82bce0d58d9b019e1ab23fcf1224a982168d80/0101-swupdate.service-remove-notify-RES.patch; then
+     patch -p1 <${SCRIPTPATH}/against-5a82bce0d58d9b019e1ab23fcf1224a982168d80/0101-swupdate.service-remove-notify-RES.patch
+fi
+
+git checkout recipes-support/swupdate/swupdate/swupdate.socket.tmpl
+if ! patch -R -p1 -s -f --dry-run <${SCRIPTPATH}/against-5a82bce0d58d9b019e1ab23fcf1224a982168d80/0102-swupdate.socket.tmpl-hardcode-ListenStream-RES.patch; then
+     patch -p1 <${SCRIPTPATH}/against-5a82bce0d58d9b019e1ab23fcf1224a982168d80/0102-swupdate.socket.tmpl-hardcode-ListenStream-RES.patch
+fi
+
+rm -rf patches-from-mailing-list
+if ! patch -R -p1 -s -f --dry-run <${SCRIPTPATH}/against-5a82bce0d58d9b019e1ab23fcf1224a982168d80/0103-links-to-patches-from-u-boot-mailing-list-RES.patch; then
+     patch -p1 <${SCRIPTPATH}/against-5a82bce0d58d9b019e1ab23fcf1224a982168d80/0103-links-to-patches-from-u-boot-mailing-list-RES.patch
+fi
+
+# <-- RES specific stuff
+
 
 git diff --stat
 
