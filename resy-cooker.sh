7f69acb9cff10f5f29f2f38a2b284a3f54d4e0ed
Robert Berger <Robert.Berger@ReliableEmbeddedSystems.com>
2026-08-15 22:36:58 +0200

    2026-08-11-01

--- resy-cooker.sh
+++ resy-cooker.sh
@@ -178,6 +178,9 @@ MYMAP[yocto-ampliphy-master-2026-04-23-01-yocto-ampliphy-pinned-distro_ampliphy-
 MYMAP[yocto-ampliphy-master-2026-05-14-01-pinned-yocto-ampliphy-pinned-distro_ampliphy-resy-systemd-machine_phyboard-pollux-imx8mp-3]="core-image-minimal core-image-sato-sdk"
 # <-- yocto-ampliphy-master-2026-05-14-01-pinned-yocto-ampliphy-pinned-distro_ampliphy-resy-systemd-machine_phyboard-pollux-imx8mp-3
 
+# --> yocto-ampliphy-master-2026-08-11-01-pinned-yocto-ampliphy-pinned-distro_ampliphy-resy-systemd-machine_phyboard-pollux-imx8mp-3
+MYMAP[yocto-ampliphy-master-2026-08-11-01-pinned-yocto-ampliphy-pinned-distro_ampliphy-resy-systemd-machine_phyboard-pollux-imx8mp-3]="core-image-minimal core-image-sato-sdk"
+# <-- yocto-ampliphy-master-2026-08-11-01-pinned-yocto-ampliphy-pinned-distro_ampliphy-resy-systemd-machine_phyboard-pollux-imx8mp-3
 
 # --> multi-v7-ml-gdbserver-master
 # jenkins:
@@ -1665,8 +1668,15 @@ fi
   # bitbake-setup:
 
   if [ "$machine" == "yocto-ampliphy-master-2026-05-14-01-pinned-yocto-ampliphy-pinned-distro_ampliphy-resy-systemd-machine_phyboard-pollux-imx8mp-3" ]; then
-     #source /workdir/build/yocto-ampliphy-master-2026-04-23-01-pinned/bitbake-builds/yocto-ampliphy-pinned/build/init-build-env
      cd /workdir/build/yocto-ampliphy-master-2026-05-14-01-pinned/bitbake-builds/yocto-ampliphy-pinned/build
+     export BUILDDIR=$(pwd)
+     source init-build-env
+  fi
+
+  # bitbake-setup:
+
+  if [ "$machine" == "yocto-ampliphy-master-2026-08-11-01-pinned-yocto-ampliphy-pinned-distro_ampliphy-resy-systemd-machine_phyboard-pollux-imx8mp-3" ]; then
+     cd /workdir/build/yocto-ampliphy-master-2026-08-11-01-pinned/bitbake-builds/yocto-ampliphy-pinned/build
      export BUILDDIR=$(pwd)
      source init-build-env
   fi
